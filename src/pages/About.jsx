c3d9a35e0ab3a4a0bf1a144dc16f9ad9091be3b6
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-07-27 12:36:14 -0700

    Modified about page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,33 +10,19 @@ const About = () => (
     </Link>
     <div className="page">
       <div className="wrapper">
-        <div className="header">
-          About.
-        </div>
+        <div className="header">About.</div>
         <div>
           This web app is a multitool for doing a bunch of things. It was built by
-          {' '}
-          <a href="https://www.jancharatan.com/">
-            Jan Charatan
-          </a>
-          , a computer science student at Pomona College using
-          {' '}
-          <a href="https://www.reactjs.org">
-            React
-          </a>
-          . If you have any suggestions for a tool that should be added or any
-          comments, email Jan at jan.charatan@gmail.com or
-          {' '}
-          <a href="https://github.com/jancharatan/calculator">submit a pull request</a>
-          .
+{' '}
+          <a href="http://www.jancharatan.com/">Jan Charatan</a>, a computer science student at Pomona College using{' '}
+          <a href="https://www.reactjs.org">React</a>. If you have any suggestions for a tool that should be added or
+          any comments, email Jan at jan.charatan@gmail.com or{' '}
+          <a href="https://github.com/jancharatan/calculator">submit a pull request</a>.
         </div>
-        <div className="footer">
-          Created in July 2020 by Jan Charatan.
-        </div>
+        <div className="footer">Created in July 2020 by Jan Charatan.</div>
       </div>
     </div>
   </div>
-
 );
 
 export default About;
