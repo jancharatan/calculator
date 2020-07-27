479201ddbf7261521af342d24b138cb95e20299d
jancharatan <52763246+jancharatan@users.noreply.github.com>
2020-07-27 13:56:37 -0700

    Updates

---
+++ precache-manifest.4a60705bed37d78d26cdaf3f2e0ce599.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b758f5cbe21846361cb137466533df5b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "3161ec0cdf3c328f5a5e",
+    "url": "/calculator/static/css/main.b6cdd5f0.chunk.css"
+  },
+  {
+    "revision": "47e77eb9f610ff66453a",
+    "url": "/calculator/static/js/2.e0dd56ce.chunk.js"
+  },
+  {
+    "revision": "ea92608dacd679b05ed806eae8cfba93",
+    "url": "/calculator/static/js/2.e0dd56ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3161ec0cdf3c328f5a5e",
+    "url": "/calculator/static/js/main.850913e4.chunk.js"
+  },
+  {
+    "revision": "ee4a148f714da062db4f",
+    "url": "/calculator/static/js/runtime-main.f0d0de70.js"
+  }
+]);
